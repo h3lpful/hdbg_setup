39965c534d12829a9bd3a34b45958274587d4d83
H3lpful <alexyander.harris@outlook.com>
2020-12-11 11:38:47 -0500

    Update hdbg_setup.sh

--- hdbg_setup.sh
+++ hdbg_setup.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
 
-#install tools
+#install add arch for 32 bit
 sudo dpkg --add-architecture i386
+
+#ensure we are up to date
 sudo apt update && upgrade -y
-sudo apt install -y openjdk-11-jdk build-essential gdb vim git ipython ipython3 tmux ruby-full apt-utils xinetd python python3 python3-pip
-sudo apt install -y libc6:i386 libncurses5:i386 libstdc++6:i386 netcat checksec gcc-multilib nasm net-tools
+
+#important system stuffs for programming
+sudo apt install -y build-essential libc6:i386 libc6-dbg:i386 libncurses5:i386 libstdc++6:i386  
+sudo apt install -y apt-utils xinetd netcat gcc-multilib nasm net-tools
+
+#all of the python things
+sudo apt install -y ipython ipython3 python python3 python3-pip 
+
+#open jdk for ghidra later
+sudo apt install -y openjdk-11-jdk openjdk-11-jre-headless
+
+#debugging and my favorite dev tools
+sudo apt install -y gdb vim tmux
+ 
+
+#ruby because reasons
+sudo apt install -y ruby-full
 sudo apt-get install rubygems
 
 #install pwndbg & other tools
 git clone https://github.com/pwndbg/pwndbg.git && cd pwndbg && ./setup.sh
 cd ..
 pip3 install pwntools regex
+
+#one_gadget for the easy rops
 sudo gem install one_gadget
 
-#get splitmind
+#splitmind for cool looking gdb
 git clone https://github.com/jerdna-regeiz/splitmind
-
 cp .gdbinit ../.gdbinit
 
+# i honestly cant remember what this is for, so ill leave it
 pip3 install pygments
 
 # install valgrind
 sudo apt install -y valgrind
-
-# ensure 32-bit libc debug lib is installed
-sudo apt install -y libc6-dbg:i386 
 
 # install wireshark
 sudo apt install -y wireshark
@@ -32,11 +48,7 @@ sudo apt install -y wireshark
 # yes - should superusers be able to capture packets
 sudo dpkg-reconfigure wireshark-common
 sudo usermod -a -G wireshark $USER
-
 # need to restart in order for non-super user packet capture
-
-# install netcat
-sudo apt install -y netcat
 
 # install ltrace
 sudo apt install -y ltrace
@@ -48,23 +60,23 @@ sudo apt install -y strace
 wget https://ghidra-sre.org/ghidra_9.2_PUBLIC_20201113.zip
 unzip ghidra_9.2_PUBLIC_20201113.zip
 rm ghidra_9.2_PUBLIC_20201113.zip
-sudo apt install -y openjdk-11-jdk
-sudo apt install -y openjdk-11-jre-headless
 sudo mv ghidra_9.2_PUBLIC /home/helpful/Desktop/ghidra
 sudo ln -s /home/helpful/Desktop/ghidra/ghidraRun /bin/ghidra
 # now typing ghidra from the cmd line will start ghidra
 
 # install checksec
-cd /bin/
 sudo wget http://www.trapkit.de/tools/checksec.sh
-sudo mv checksec.sh checksec
-sudo chmod +x checksec
+sudo mv checksec.sh /usr/bin/checksec
+sudo chmod +x /usr/bin/checksec
+rm checksec.sh
 
 #install intel manual
-https://software.intel.com/content/dam/develop/public/us/en/documents/325462-sdm-vol-1-2abcd-3abcd.pdf
+wget -O intel_manual.pdf https://software.intel.com/content/dam/develop/public/us/en/documents/325462-sdm-vol-1-2abcd-3abcd.pdf
 
 #install python documentation
-wget -O pydocs.zip https://docs.python.org/3.8/archives/python-3.8.5-docs-html.zip && unzip pydocs.zip && rm pydocs.zip
+wget -O pydocs.zip https://docs.python.org/3.8/archives/python-3.8.5-docs-html.zip
+unzip pydocs.zip
+rm pydocs.zip
 
 # reboot
 sudo reboot
